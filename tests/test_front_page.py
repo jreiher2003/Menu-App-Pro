572919f0e5dba647b55c4dc16dcb9daa3e21f1bb
Jeff Reiher <jreiher2003@yahoo.com>
2016-02-24 12:20:18 -0500

    Add tests to test front page

--- tests/test_front_page.py
+++ tests/test_front_page.py
@@ -1,23 +1,34 @@
 import unittest 
 from base import BaseTestCase 
 from app import app,db 
+from flask import request
 from app.models import Place
 
 class TestFrontPage(BaseTestCase):
 
     def test_index_loads(self):
-        response = self.client.get('/', content_type='html/text')
-        self.assertEqual(response.status_code, 200)
-        self.assertIn(b'Testname', response.data)
+        response = self.client.get("/", follow_redirects=True)
+        assert response.status_code == 200
+        assert "Testname" in response.data
 
     def test_stylesheet_loads(self):
-        response = self.client.get('/static/styles.css', content_type='html/text')
-        self.assertEqual(response.status_code, 200)
+        response = self.client.get("/static/styles.css")
+        assert response.status_code == 200
 
     def test_place_database(self):
     	place = Place.query.filter_by(id=1).one()
-    	self.assertEqual(place.id, 1)
-    	self.assertEqual(place.name, 'Testname')
+    	assert place.id == 1
+        assert place.name == "Testname"
+
+    def test_request_url(self):
+        with app.test_request_context("/", method="GET"):
+            assert request.path == "/"
+            assert request.method == "GET"
+
+
+    
+
+            
 
 
 
