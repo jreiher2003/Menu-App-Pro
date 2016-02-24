2dd74f6f9f6b4bd0134111af6078c2a5539c2c60
Jeff Reiher <jreiher2003@yahoo.com>
2016-02-24 11:26:14 -0500

    Add coverage

--- app/views.py
+++ app/views.py
@@ -1,8 +1,8 @@
 from app import app,db # pragma: no cover
-from app.models import Place
+from app.models import Place # pragma: no cover
 from flask import render_template # pragma: no cover
 
-@app.route('/') # pragma: no cover
+@app.route('/') 
 def index():
 	all_places = Place.query.all()
 	return render_template('index.html', all_places=all_places)
