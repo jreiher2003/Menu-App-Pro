067a65ff6c75826af79037958bfed86b07dfbe19
Jeff Reiher <jreiher2003@yahoo.com>
2016-02-24 15:42:26 -0500

    Try to get 100% coverage

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ from app import app,db # pragma: no cover
 from app.models import Place # pragma: no cover
 from flask import render_template # pragma: no cover
 
-@app.route('/') # pragma: no cover
+@app.route('/') 
 def index(): 
 	all_places = Place.query.all()
 	return render_template('index.html', all_places=all_places)
